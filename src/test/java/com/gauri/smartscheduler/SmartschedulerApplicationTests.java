9639e4acc9983d97b035806ece6da19a8aacdde4
20069222 <gauriwadatkar9@gmail.com>
2026-02-18 01:30:36 +0530

    Initial backend setup

---
+++ src/test/java/com/gauri/smartscheduler/SmartschedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gauri.smartscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartschedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
